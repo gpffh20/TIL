cab588280f0e1652fda1e650ca14eceb0fd62b7e
gpffh20 <eunbin584836@gmail.com>
2022-11-17 11:01:28 +0900

    BOJ_큐

---
+++ C:C++/BOJ/10845_큐.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    queue <int> Q;
+    int n, i;
+    string s;
+    cin >> n;
+    while(n--)
+    {
+        cin >> s;
+        if(s == "push")
+        {
+            cin >> i;
+            Q.push(i);
+        }
+        else if(s == "pop")
+        {
+            if(Q.empty())
+                cout << -1 << '\n';
+            else
+            {
+                cout << Q.front() << '\n';
+                Q.pop();
+            }
+        }
+        else if(s == "size")
+            cout << Q.size() << '\n';
+        else if(s == "empty")
+            cout << Q.empty() << '\n';
+        else if(s == "front")
+        {
+            if(Q.empty())
+                cout << -1 << '\n';
+            else
+                cout << Q.front() << '\n';
+        }
+        else    // back
+        {
+            if(Q.empty())
+                cout << -1 << '\n';
+            else
+                cout << Q.back() << '\n';
+        }
+    }
+}

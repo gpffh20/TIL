f06c996b292951ca4d36b72db190af863981fcc7
gpffh20 <eunbin584836@gmail.com>
2022-12-03 23:47:54 +0900

    BOJ_A+B_8

---
+++ C:C++/BOJ/11022_A+B_8.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, a, b, i = 1;
+    cin >> n;
+    while(n--)
+    {
+        cin >> a >> b;
+        cout << "Case #" << i << ": " << a;
+        cout << " + " << b << " = " << a + b << '\n';
+        i++;
+    }
+}
